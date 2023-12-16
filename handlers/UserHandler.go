f57dfffae859f90af346763dd49c9e5de4e54c24
Faris Fadhail <farisfadhail@gmail.com>
2023-12-17 00:25:38 +0700

    feat: setup user handler

---
+++ handlers/UserHandler.go
@@ -0,0 +1,19 @@
+package handlers
+
+import "github.com/gofiber/fiber/v2"
+
+func GetAllUserHandler(ctx *fiber.Ctx) error {
+	panic("udin")
+}
+
+func GetByIdUserHandler(ctx *fiber.Ctx) error {
+	panic("udin")
+}
+
+func UpdateUserHandler(ctx *fiber.Ctx) error {
+	panic("udin")
+}
+
+func DeleteUserHandler(ctx *fiber.Ctx) error {
+	panic("udin")
+}
